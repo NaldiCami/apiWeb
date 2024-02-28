39077b3a790a6897d137025eb9a1fcdeec4161a4
txKaue <kauetxp@gmail.com>
2024-02-28 20:54:55 -0300

    primeiro commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "c4b63b373amshfdc89f509c67fffp19cb64jsn0424e9c40d9b",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "88f1dbcb341ef664d8c69acb794794b0"; // enter your key from openweather API
